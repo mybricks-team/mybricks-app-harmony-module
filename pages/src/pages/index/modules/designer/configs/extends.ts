bfe5f95d59543a4deafe871f5a005ced17234640
leon-js <42924541+leon-js@users.noreply.github.com>
2025-05-23 11:11:25 +0800

    init

--- pages/src/pages/index/modules/designer/configs/extends.ts
+++ pages/src/pages/index/modules/designer/configs/extends.ts
@@ -1,60 +1,29 @@
 export default {
-  title: "生命周期",
   cards: [
-    // {
-    //   id: "appOnLaunch",
-    //   title: "应用启动",
-    //   inputs: [
-    //     {
-    //       id: "params",
-    //       title: "请求描述",
-    //       schema: {
-    //         type: "object",
-    //         properties: {
-    //           params: {
-    //             type: "object",
-    //             description: "启动参数",
-    //           },
-    //         },
-    //       },
-    //     },
-    //   ],
-    // },
     {
-      id: "pageOnLoad",
-      title: "当页面加载时",
+      id: 'openRouter',
+      title: '业务模块',
+      required: true,
       inputs: [
         {
-          id: "onLoad",
-          title: "页面加载",
+          id: 'params',
+          title: '打开页面',
           schema: {
-            type: "object",
+            type: 'object',
             properties: {
-              id: {
-                type: "string",
-                description: "当前页面 ID",
+              uri: {
+                type: 'string',
+                description: '页面路径'
               },
-              path: {
-                type: "string",
-                description: "当前页面路径",
-              },
-              query: {
-                type: "object",
-                description: "当前页面参数",
-              },
-            },
-          },
-        },
+              params: {
+                type: 'object',
+                description: '请求参数'
+              }
+            }
+          }
+        }
       ],
-      outputs: [
-        {
-          id: "next",
-          title: "继续",
-          schema: {
-            type: "any",
-          },
-        },
-      ],
+      outputsEditable: true,
     },
   ],
 };
