98f06b41c17a0d1969193b7e1eb70391ec0fe34a
leon-js <42924541+leon-js@users.noreply.github.com>
2025-05-27 21:02:07 +0800

    update

--- nodejs/module/compile/compiler/harmony2.ts
+++ nodejs/module/compile/compiler/harmony2.ts
@@ -1,7 +1,6 @@
 import toHarmonyCode from "@mybricks/to-code-react/dist/cjs/toHarmonyCode"
 import * as path from "path";
 import * as fse from "fs-extra";
-import { BaseCompiler } from "./base";
 import { COMPONENT_PACKAGE_NAME } from "./hm/constant";
 import { pinyin, cleanAndSplitString, firstCharToUpperCase } from "../utils";
 
@@ -176,16 +175,7 @@ const handlePopupCode = (page: ReturnType<typeof toHarmonyCode>[0]) => {
 }
 
 export const compilerHarmony2 = async (params, config) => {
-
-  const { data, projectPath, projectName, fileName, depModules, origin, type } = params;
-
-  if (params.type === "harmonyApplication") {
-    // 下载应用
-    await compilerHarmonyApplication(params, config)
-  } else {
-    // 下载组件
-    await compilerHarmonyComponent(params, config)
-  }
+  await compilerHarmonyModule(params, config)
 }
 
 const generatePageFileName = (text: string) => {
@@ -242,114 +232,8 @@ const getPageCode = (toJson) => {
   });
 }
 
-/** 下载应用 */
-const compilerHarmonyApplication = async (params, config) => {
-  const { data, projectPath, projectName, fileName, depModules, origin, type } = params;
-  const { Logger } = config;
-  const pageCode = getPageCode(data.toJson);
-
-  // 目标项目路径
-  const targetAppPath = path.join(projectPath, "Application");
-
-  // 拷贝项目
-  await fse.copy(path.join(__dirname, "./hm/Application"), targetAppPath, { overwrite: true })
-  // 拷贝comlib
-  await fse.copy(path.join(__dirname, "./hm/comlib"), path.join(targetAppPath, "entry/src/main/comlib"), { overwrite: true })
-  // est路径
-  const targetEtsPath = path.join(targetAppPath, "entry/src/main/ets");
-  // 拷贝utils
-  await fse.copy(path.join(__dirname, "./hm/utils"), path.join(targetEtsPath, "utils"), { overwrite: true })
-  // 拷贝_proxy
-  await fse.copy(path.join(__dirname, "./hm/_proxy"), path.join(targetEtsPath, "_proxy"), { overwrite: true })
-
-  const proxyFile = path.join(targetEtsPath, "_proxy", 'Index.ets');
-  const proxyFileContent = await fse.readFile(proxyFile, 'utf-8');
-  await fse.writeFile(proxyFile, proxyFileContent.replace('{ domain: undefined }', `{ domain: ${data.appConfig?.defaultCallServiceHost ? JSON.stringify(data.appConfig?.defaultCallServiceHost) : undefined}}`), 'utf-8')
-
-  const sceneMap = {};
-
-  pageCode.forEach((page) => {
-    if (page.meta) {
-      sceneMap[page.meta.id] = page.meta;
-    }
-
-    let content = "";
-    if (page.type === "ignore") {
-      // 不做特殊处理，一般是固定模版代码
-      content = page.content;
-    } else if (page.type === "normal") {
-      const { pageConfig } = data.pages.find(p => p.id === page.meta?.id) ?? {}
-      // 页面
-      content = handlePageCode(page, pageConfig);
-    } else if (page.type === "popup") {
-      // 弹窗
-      content = handlePopupCode(page);
-    }
-
-    fse.outputFileSync(path.join(targetEtsPath, page.path), content, { encoding: "utf8" })
-  });
-
-  // 写入搭建Js
-  const jsCodePath = path.join(targetEtsPath, "_proxy/codes.js");
-  await fse.ensureFile(jsCodePath)
-  await fse.writeFile(jsCodePath, `export default (function(comModules) {
-    ${decodeURIComponent(data.allModules?.all)};
-    return comModules;
-  })({})`, { encoding: "utf8" })
-
-  // tabbar配置
-  const tabbarConfig = (data.tabBarJson ?? []).map(item => {
-    const { pagePath, ...others } = item
-    return {
-      id: item.pagePath.split('/')[1],
-      ...others,
-    }
-  })
-
-  // 入口场景
-  const entryScene = sceneMap[data.entryPageId]
-
-  // tabbar场景
-  const tabbarScenes: string[] = data.pages.filter(p => 
-    (data.tabBarJson || []).some(
-      (b) => b?.id === p?.id
-    )
-  ).map(p => {
-    return sceneMap[p.id]
-  })
-
-  // 普通场景
-  const normalScenes: string[] = data.pages.filter(p => 
-    !(data.tabBarJson || []).some(
-      (b) => b?.id === p?.id
-    )
-  ).map(p => {
-    return sceneMap[p.id]
-  })
-
-  // 弹窗也写入普通场景判断中
-  data.toJson.scenes.forEach((scene) => {
-    if (scene.type === "popup") {
-      normalScenes.push(sceneMap[scene.id])
-    }
-  })
-
-  const entryPath = path.join(targetEtsPath, "./pages/Index.ets");
-  await fse.copy(path.join(__dirname, "./hm/pages/Index.ets"), entryPath, { overwrite: true });
-  
-  let entryFileContent = await fse.readFile(entryPath, 'utf-8')
-
-  entryFileContent = handleEntryCode(entryFileContent, {
-    normalScenes,
-    tabbarScenes,
-    tabbarConfig,
-    entryScene
-  })
-  await fse.writeFile(entryPath, entryFileContent, 'utf-8')
-}
-
-/** 下载组件 */
-const compilerHarmonyComponent = async (params, config) => {
+/** 下载模块 */
+const compilerHarmonyModule = async (params, config) => {
   const { data, projectPath, projectName, fileName, depModules, origin, type, fileId } = params;
   const { Logger } = config;
   const pageCode = getPageCode(data.toJson);
