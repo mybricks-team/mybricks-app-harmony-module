b60ae89f6e1fb223bbb53c8bc1e8d62c17a74554
leon-js <42924541+leon-js@users.noreply.github.com>
2025-12-04 15:32:26 +0800

    update

--- pages/src/pages/index/modules/designer/utils/get-ai-encrypt-data.ts
+++ pages/src/pages/index/modules/designer/utils/get-ai-encrypt-data.ts
@@ -2,7 +2,7 @@ import { Utils } from '@mybricks/sdk-for-ai'
 
 export const aiUtils = {
   get getAiEncryptData () {
-    if (window._ai_test_) {
+    if (window._ai_test_ || localStorage.getItem("_ai_test_")) {
       return (data) => data
     }
     return Utils.getAiEncryptData
